968280b5c794d644e499b89bed95502ca849c5e2
quickslvrcoding <leezcreed@gmail.com>
2021-08-22 22:06:44 -0500

    Update yrgury.py

--- yrgury.py
+++ yrgury.py
@@ -1,10 +1,12 @@
 name = 'Supenia'
-age = 5000
+age = 5001
 if name == 'Yrgury':
     print('Hi Yrgury')
 elif age < 25:
     print('You are not Yrgury')
 elif age > 1000 and age < 5000:
     print('You are old, but you are not the ancient one!')
-elif age > 4000:
+elif age > 4000 and age < 5000:
     print('You must be the ancient once known as Yrgury!')
+else:
+    print("Seems like Yrgury is not here. I must continue my search....")
