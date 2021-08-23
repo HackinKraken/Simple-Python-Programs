ac904458b306a14ed93d3c19b45fd7cdb10f77dd
quickslvrcoding <leezcreed@gmail.com>
2021-08-22 22:03:15 -0500

    Create yrgury.py

---
+++ yrgury.py
@@ -0,0 +1,10 @@
+name = 'Supenia'
+age = 5000
+if name == 'Yrgury':
+    print('Hi Yrgury')
+elif age < 25:
+    print('You are not Yrgury')
+elif age > 1000 and age < 5000:
+    print('You are old, but you are not the ancient one!')
+elif age > 4000:
+    print('You must be the ancient once known as Yrgury!')
